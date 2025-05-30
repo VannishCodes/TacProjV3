60b97f56acea4d4211fb2e483390afbbd00f53a3
Vannish <vannishcoach@gmail.com>
2025-05-30 05:08:13 +0100

    Added Tile Info UI

---
+++ scenes/UI/tile_info_ui.gd
@@ -0,0 +1,18 @@
+extends PanelContainer
+
+func _on_cursor_on_move(cell : Vector2i) -> void:
+	get_tile_info(cell)
+
+func get_tile_info(cell : Vector2i) -> void:
+	var tile_map : TileMapLayer
+	tile_map = get_tree().get_first_node_in_group("map_tiles")
+	
+	if not tile_map:
+		return
+	
+	cell = tile_map.local_to_map(cell)
+	var tile_data : TileData = tile_map.get_cell_tile_data(cell)
+	
+	if tile_data:
+		var tile_name : StringName = tile_data.get_custom_data("tile_name")
+		$VBoxContainer/Label.text = tile_name
--- scenes/cursor/cursor.gd
+++ scenes/cursor/cursor.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal on_move
+
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_released():
 		return
@@ -12,8 +14,6 @@ func _unhandled_key_input(event: InputEvent) -> void:
 		move_cursor(Vector2i.LEFT)
 	elif event.is_action("right"):
 		move_cursor(Vector2i.RIGHT)
-		
-	get_tile_info()
 
 func move_cursor(direction : Vector2i) -> void:
 	var tile_map : TileMapLayer
@@ -28,17 +28,4 @@ func move_cursor(direction : Vector2i) -> void:
 	if $"..".is_in_boundsv(cell):
 		var global_direction = Vector2(direction) * Globals.TILE_SIZE
 		global_position += global_direction
-
-func get_tile_info() -> void:
-	var tile_map : TileMapLayer
-	tile_map = get_tree().get_first_node_in_group("map_tiles")
-	
-	if not tile_map:
-		return
-	
-	var cell : Vector2i = tile_map.local_to_map(self.position)
-	var tile_data : TileData = tile_map.get_cell_tile_data(cell)
-	
-	if tile_data:
-		var tile_name : StringName = tile_data.get_custom_data("tile_name")
-		print(tile_name)
+		on_move.emit(global_position)
