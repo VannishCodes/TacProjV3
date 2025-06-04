68d43bac7e66a77cbb35fc598c617bc840a5bf6e
Vannish <vannishcoach@gmail.com>
2025-06-04 13:04:38 +0100

    Added basic character stats and wait_time formula

--- resources/character_stats/character_stats.gd
+++ resources/character_stats/character_stats.gd
@@ -8,8 +8,32 @@ class_name CharacterStats
 @export var playable : bool = false
 
 #Basic Stats
-#Variable below is temporary
-@export var wait_timer : int = 20
+@export var maximum_health_points : int
+@export var maximum_mana_points : int
+@export var strength : int
+@export var magic : int
+@export var defense : int
+@export var resistance : int
+@export var dexterity : int
+@export var agility : int
+@export var speed : int = 20
 @export var movement_speed : int = 4
+@export var elemental_affinity : Globals.ELEMENTS
+
+#Complex Stats
+var wait_timer : int
+
+
+#Other Stats
 @export var min_attack_range : int = 1
 @export var max_attack_range : int = 1
+
+func initialize_complex_stats() -> void:
+	calculate_wait_timer()
+
+func calculate_wait_timer() -> void:
+	var unrounded_result : float
+	unrounded_result = 100.0 * (20.0 / (20.0 + speed))
+	print(unrounded_result)
+	wait_timer = int(unrounded_result)
+	print(wait_timer)
--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -8,6 +8,7 @@ class_name Character
 signal turn_ended
 
 func _ready() -> void:
+	stats.initialize_complex_stats()
 	animated_sprite.sprite_frames = stats.sprite_frames
 	animated_sprite.play("default")
 
